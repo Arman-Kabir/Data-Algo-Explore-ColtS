011a6cad791592394f66c706b4a75d57f878b592
Arman-Kabir <armankabir8@gmail.com>
2023-01-24 11:37:33 +0600

    intro bst

---
+++ 22-Binary Search Trees/4-intro-binary-trees.js
@@ -0,0 +1,15 @@
+/*Kinds of trees*********
+*Trees
+*Binary trees - Each node can have at most two children at the binary part. Not three
+*binary search trees- are a special case of a binary tree.
+
+**In BST - all numbers less than the root goes to the left side- 
+    **others(greater than root) go to the right side. We could repeat that on each child node
+
+****How bst works*****
+*Every parent node has at most 2 children
+* Every node to the left of a parent node is always less than the parent
+* Every node to the right of a parent node is always greater than the parent
+
+
+*/
