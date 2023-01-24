a7f230ca04e91a61fa427431c44c58deba01e789
Arman-Kabir <armankabir8@gmail.com>
2023-01-24 09:57:53 +0600

    tree basic

---
+++ 22-Binary Search Trees/2.js
@@ -0,0 +1,14 @@
+/*What is a tree ?
+**A data structure that consists of nodes (just like linked list did) in a parent/child relationship.
+**Each node can point to more than one or can have more than one reference to another node
+**One root node- there is only one entry point
+**Lists - linear.. Trees- non-linear.. 
+*In trees - thers's many different paths we could take
+*In lists- there's only one pathway through
+
+**In a tree - every node is moving away from the root node in a parent-child relationship
+
+**Root- The top node in a tree
+**Child - A node directly connected to another node when moving away from the root
+
+*/
