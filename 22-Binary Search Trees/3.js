e88425dec98b5e7324f52dfd26e1ab2bda3b6104
Arman-Kabir <armankabir8@gmail.com>
2023-01-24 11:15:39 +0600

    uses

---
+++ 22-Binary Search Trees/3.js
@@ -0,0 +1,11 @@
+/*  Uses for trees*******
+***Lost of different applications!
+*HTML DOM
+*Network Routing
+*Abstract Syntax trees
+*Artificial Intelligence
+*Folders in Operating Syatem
+*Computers File Syatem
+
+
+*/
