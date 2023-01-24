2ca07cac8e1234ffc3a3d22d728d1854c6810088
Arman-Kabir <armankabir8@gmail.com>
2023-01-24 11:45:05 +0600

    searching bst done

---
+++ 22-Binary Search Trees/6-searching a bst.js
@@ -0,0 +1,5 @@
+/* 
+***Everything to the right is greater, everything to the left is smaller...
+**Binary search tree works on sorted data where there'e an order
+**We can find things much faster
+*/
