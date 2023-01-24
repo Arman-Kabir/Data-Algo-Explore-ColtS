e78700d10c3572b3e5e0c08e3b7e82103f8d867f
Arman-Kabir <armankabir8@gmail.com>
2023-01-24 14:39:06 +0600

    bst insert done

---
+++ 22-Binary Search Trees/9-bst-insert-solution.js
@@ -0,0 +1,60 @@
+/*
+        10
+    5       13
+  2   7   11  16
+*/
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    // inserting a node
+    insert(value) {
+        var newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            var current = this.root;
+            while (true) {
+                if (value === current.value) return undefined;
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+        // return "can't process";
+    }
+}
+
+var tree = new BinarySearchTree();
+console.log(tree.insert(10));
+console.log(tree.insert(5));
+console.log(tree.insert(13));
+console.log(tree.insert(11));
+console.log(tree.insert(16));
+console.log(tree.insert(7));
+console.log(tree.insert(2));
+console.log(tree.insert(10));
+console.log(tree.insert(7));
+console.log(tree);
