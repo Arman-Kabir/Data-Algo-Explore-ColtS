fcbb9668179e3664f2290558e99c1d7c01eb97a4
Arman-Kabir <armankabir8@gmail.com>
2023-03-05 18:52:03 +0600

    sort intro

---
+++ 11-Bubble-sort/2-3.js
@@ -0,0 +1,27 @@
+
+// 2 Intro
+/*
+   What is sorting ?
+   * Sorting is the procss of re-arranging the items in a collection(e.g. an array) so that the items
+    are in some kind of order. 
+
+    *Examples
+    1.
+
+     *Sorting is an incredibly common task, so it's good to know how it works.
+
+     //Elementary sorting algorithms
+     *Bubble Sort
+     *Selection Sort
+     *Insertion Sort
+
+     function sort(arr) {
+         return arr;
+     }
+     
+     sort([23, 45, 6, 12, 13]);
+*/
+
+
+
+// 3- Js Sort Method
