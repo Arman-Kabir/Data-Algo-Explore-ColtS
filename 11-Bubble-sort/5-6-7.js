7bcb597930d890909f25e2dce49070bbaa9e016a
Arman-Kabir <armankabir8@gmail.com>
2023-03-06 13:01:59 +0600

    bubble sort modified

--- 11-Bubble-sort/5-6-7.js
+++ 11-Bubble-sort/5-6-7.js
@@ -1,10 +1,29 @@
 
 // 5 - Implementation
+// function bubbleSort(arr) {
+//     for (var i = 0; i < arr.length; i++) {
+//         // console.log(arr[i]);
+//         for (var j = 0; j < arr.length; j++) {
+//             console.log(arr,i,arr[j],arr[j+1]);
+//             if (arr[j] > arr[j + 1]) {
+//                 // swap
+//                 var temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(bubbleSort([37, 45, 29, 8]));
+
+
 function bubbleSort(arr) {
-    for (var i = 0; i < arr.length; i++) {
+    for (var i = arr.length; i > 0; i--) {
         // console.log(arr[i]);
-        for (var j = 0; j < arr.length; j++) {
-            console.log(arr);
+        for (var j = 0; j < i-1; j++) {
+            console.log(arr, i, arr[j], arr[j + 1]);
             if (arr[j] > arr[j + 1]) {
                 // swap
                 var temp = arr[j];
